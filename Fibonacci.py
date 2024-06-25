35825aa01fbf0ef13f973f55579707680eecfe83
Chaitra B R <149990777+chaitrabr08@users.noreply.github.com>
2024-06-25 23:07:54 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the num:"))
+def fibonacci(i):
+        if i==0:
+                return 0
+        elif i==1:
+                return 1
+        else:
+                return fibonacci(i-2)+fibonacci(i-1)
+for x in range(n):
+        print(fibonacci(x))
