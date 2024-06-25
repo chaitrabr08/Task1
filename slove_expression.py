bcf4e1aec08ed73d759976134dc4cc9c779715d7
Chaitra B R <149990777+chaitrabr08@users.noreply.github.com>
2024-06-25 23:22:46 +0530

    Add files via upload

---
+++ slove_expression.py
@@ -0,0 +1,7 @@
+def solve_expression(expression):
+        try:
+                result = eval(expression)
+                return result
+        except Exception as e:
+                return str(e)
+
